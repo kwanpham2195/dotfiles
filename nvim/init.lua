693772961fdfeea2de1c0660d3580be71e7dcca3
Quan Minh Pham <quanpm.mta@gmail.com>
2023-02-16 17:46:49 +0700

    remove submodule

---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+-- References to ./lua/
+
+-- Load global functions
+require("core.globals")
+-- Plugin management via Packer
+require("core.packer")
+-- "Global" Keymappings
+require("core.mappings")
+-- All non plugin related (vim) options
+require("core.options")
+-- Vim autocommands/autogroups
+require("core.autocmd")
