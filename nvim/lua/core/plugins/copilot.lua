b9bfdd82406669ba5cc7ada376da0dc6ff5cb4f1
Quan Minh Pham <quanpm.mta@gmail.com>
2023-03-29 10:20:46 +0700

    feat: add support for copilot

--- nvim/lua/core/plugins/copilot.lua
+++ nvim/lua/core/plugins/copilot.lua
@@ -1,5 +1,19 @@
 local M = {
-  "github/copilot.vim",
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<right>",
+          next = "<up>",
+          prev = "<down>",
+        },
+      },
+    })
+  end,
 }
 
 return M
